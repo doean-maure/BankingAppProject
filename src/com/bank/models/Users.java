79211a0fa9a81870cc89b817c3a34748ca2ca0c4
Doean Francimae Gilyn Maure <gilynmaure@gmail.com>
2026-06-06 16:22:36 +0800

    update Users class constructor

--- src/com/bank/models/Users.java
+++ src/com/bank/models/Users.java
@@ -5,19 +5,13 @@ public class Users {
     String mobileNum;
     int pin;
     String name;
-    String address;
-    double balance;
-    String role;
 
     //Constructor
-    public Users(int id, String mobileNum, int pin, String name, String address, double balance, String role) {
+    public Users(int id, String mobileNum, int pin, String name) {
         this.id = id;
         this.mobileNum = mobileNum;
         this.pin = pin;
         this.name = name;
-        this.address = address;
-        this.balance = balance;
-        this.role = role;
     }
 
     //Getters
@@ -25,17 +19,4 @@ public class Users {
     public String getMobileNum() { return mobileNum; }
     public int getPin() { return pin; }
     public String getName() { return name; }
-    public String getAddress() { return address; }
-    public double getBalance() { return balance; }
-    public String getRole() { return role; }
-
-
-    //Setters
-    public void setId(int id) { this.id = id; }
-    public void setMobileNum(String mobileNum) { this.mobileNum = mobileNum; }
-    public void setPin(int pin) { this.pin = pin; }
-    public void setName(String name) { this.name = name; }
-    public void setAddress(String address) { this.address = address; }
-    public void setBalance(double balance) { this.balance = balance; }
-    public void setRole(String role) { this.role = role; }
 }
