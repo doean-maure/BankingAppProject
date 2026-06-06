cef8353de8b37314300673cfec2c7893dd5ec211
Doean Francimae Gilyn Maure <gilynmaure@gmail.com>
2026-06-06 16:23:03 +0800

    update Customers class constructor

--- src/com/bank/models/Customer.java
+++ src/com/bank/models/Customer.java
@@ -4,11 +4,11 @@ public class Customer extends Users {
     // Connects this specific customer to their own bank account
     public BankAccount account;
 
-    public Customer(int id, String mobileNum, int pin, String name, String address, double balance) {
-        super(id, mobileNum, pin, name, address, balance, "Customer");
+    public Customer(int id, String mobileNum, int pin, String name) {
+        super(id, mobileNum, pin, name);
 
         // New BankAccount
-        this.account = new BankAccount(balance);
+        this.account = new BankAccount(0);
     }
 }
 
