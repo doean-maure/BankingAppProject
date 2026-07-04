537d2821315d954bdfd8824f198729622057f41e
Doean Francimae Gilyn Maure <gilynmaure@gmail.com>
2026-07-05 02:55:04 +0800

    updates error message

--- src/com/bank/models/BankAccount.java
+++ src/com/bank/models/BankAccount.java
@@ -13,59 +13,52 @@ public class BankAccount {
 
     // Feature 1 (Customer): Check Balance
     public void balance() {
-        System.out.println("Your Balance: P" + balance);
-        System.out.println("------------------------------------------------");
+        System.out.println("\nBALANCE: P" + balance);
     }
 
     // Feature 2 (Customer): Deposit
     public void deposit(double amount) {
         if (amount > 0) {
             balance += amount;
-            history.add("Cash-in: +P" + amount);
-            System.out.println("------------------------------------------------");
-            System.out.println("Cash-in successfully!\nYour balance: P" + balance);
-            System.out.println("------------------------------------------------");
+            history.add("Deposit: +P" + amount);
+            System.out.println("\nUPDATED BALANCE: P" + balance);
         } else {
-            System.out.println("------------------------------------------------");
-            System.out.println("Invalid amount.");
-            System.out.println("------------------------------------------------");
+            System.out.println("\n***INVALID AMOUNT***\n");
         }        
     }
-    
-    // Feature 3 (Customer): Transfer Money
-    public void transferMoney(BankAccount targeAccount, double amount, String sender, String receiver) {
-        if (amount > 0 && amount <= balance) {
-            this.balance -= amount;
-            this.history.add("Sent: -P" + amount + " To: " + receiver);
-            targeAccount.balance += amount;
-            targeAccount.history.add("Received: +P" + amount + " From: " + sender);
-            System.out.println("------------------------------------------------");
-            System.out.println("\t\tTRANSFER SUCCESS");
-            System.out.println("------------------------------------------------");
-            System.out.println("You have successfully sent P" + amount + " to " + receiver);
-            System.out.println("Your balance: P" + this.balance);
-            System.out.println("------------------------------------------------");
-        } else {
-            System.out.println("------------------------------------------------");
-            System.out.println("Insufficient funds or invalid amount.");
-            System.out.println("------------------------------------------------");
-        }
-    }
-
-    // Feature 4 (Customer): Withdraw
-    public void withdraw(double amount, String name, String mobileNum) {
+     // Feature 4 (Customer): Withdraw
+    public void withdraw(double amount) {
         if (amount > 0 && amount <= balance) {
             balance -= amount;
             history.add("Withdrew: -P" + amount);
-            System.out.println("------------------------------------------------");
-            System.out.println("Updadated Balance: P" + balance);
-            System.out.println("------------------------------------------------");
+            System.out.println("\nUPDATED BALANCE: P" + balance);
         } else {
-            System.out.println("------------------------------------------------");
-            System.out.println("Insufficient funds or invalid amount.");
-            System.out.println("------------------------------------------------");
+            System.out.println("\n***INSUFFICIENT FUNDS.***\n");
+        }
+        
+    }
+    // Feature 3 (Customer): Transfer Money
+    public void transferMoney(BankAccount targetAccount, double amount, String receiver, String sender) {
+
+        if (amount > 0 && amount <= balance) {
+            this.balance -= amount;
+            this.history.add("Sent: -P" + amount + " To: " + receiver);
+
+            targetAccount.balance += amount;
+            targetAccount.history.add("Received: +P" + amount + " From: " + sender);
+
+            System.out.println("\nYOU HAVE SUCCESSFULLY SENT P" + amount + " TO " + receiver);
+            System.out.println("YOUR UPDATED BALANCE: P" + this.balance);
+        
+        } else {
+           
+            System.out.println("\n***INSUFFICIENT AMOUNT OR INVALID AMOUNT***\n");
         }
     }
+    
+
+
+   
 
     // Feature 5 (Customer): Transaction History
     public void showHistory(String name, String mobile) {
@@ -74,6 +67,7 @@ public class BankAccount {
             System.out.println(record);
         }
         System.out.println("Final Balance: P" + balance);
-        System.out.println("------------------------------------------------");
     }
+
+    
 }
