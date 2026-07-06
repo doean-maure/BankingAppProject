f2fea562f3c6f6d082f36f8ca70b773e5c13361c
Doean Francimae Gilyn Maure <gilynmaure@gmail.com>
2026-07-07 04:54:20 +0800

    refactor: add List for accounts

--- src/com/bank/models/Customer.java
+++ src/com/bank/models/Customer.java
@@ -1,15 +1,21 @@
 package com.bank.models;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Customer extends Users {
-    // Connects this specific customer to their own bank account
-    public BankAccount account;
+    private List<BankAccount> accounts; 
 
     public Customer(int id, String mobileNum, int pin, String name) {
-        super(id, mobileNum, pin, name);
-
-        // New BankAccount
-        this.account = new BankAccount(0);
+        super(id, mobileNum, pin, name);    
+        
+        this.accounts = new ArrayList<>();
     }
 
+    public void addAccount(BankAccount account) {
+        this.accounts.add(account);
+    }
+
+    // Getters
+    public List<BankAccount> getAccounts() { return accounts; }
 }
 
