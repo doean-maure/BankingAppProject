5a7d0df091bcab4f94afe48cfaa1261a69ba23ff
Doean Francimae Gilyn Maure <gilynmaure@gmail.com>
2026-07-07 17:02:56 +0800

    feat(account type): create CheckingAccount class

---
+++ src/com/bank/models/CheckingAccount.java
@@ -0,0 +1,7 @@
+package com.bank.models;
+
+public class CheckingAccount extends BankAccount {
+    public CheckingAccount(String accountNumner, double initialBalance) {
+        super(accountNumner, initialBalance);
+    }
+}
