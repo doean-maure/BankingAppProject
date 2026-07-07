f8f3465b6ac4fd5a1ee0e462480b126424be7cc0
Doean Francimae Gilyn Maure <gilynmaure@gmail.com>
2026-07-07 16:52:46 +0800

    feat: update feature for account display

--- src/com/bank/models/Admin.java
+++ src/com/bank/models/Admin.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class Admin extends Users{
 
-   public BankAccount account;
+   public BankAccount accounts;
 
     public Admin(int id, String mobileNum, int pin, String name) {
         super(id, mobileNum, pin, name);
@@ -13,20 +13,29 @@ public class Admin extends Users{
     // Viewing of All Accounts
     public void viewAll(List<Users> userList) {
         System.out.println("CUSTOMER ACCOUNTS:\n");
-        System.out.println("FULL NAME\tMOBILE NUMBERS\tBALANCE");
+        System.out.println("FULL NAME\tACCOUNT NUMBERS\t\tBALANCE");
         
         for (Users user : userList) {
             if (user instanceof Customer) {
                 Customer customer = (Customer) user;
-                System.out.println(customer.name + "\t" + customer.mobileNum + "\t" + customer.account.balance);
+                for (BankAccount acc : customer.getAccounts()) { 
+                    System.out.println(customer.name + "\t" + acc.getAccountNumber() + "\t\t" + acc.balance);
+                }
             }
         }
+
+        
     }
+
+    
 
     // Viewing of Specific Account
     public void view(Customer customer) {
         System.out.println("ACCOUNT RESULT:\n");
-        System.out.println("FULL NAME\tMOBILE NUMBERS\tBALANCE");
-        System.out.println(customer.name + "\t" + customer.mobileNum + "\t" + customer.account.balance);
+        System.out.println("FULL NAME\tACCOUNT NUMBERS\t\tBALANCE");
+        
+        for (BankAccount acc : customer.getAccounts()) { 
+            System.out.println(customer.name + "\t" + acc.getAccountNumber() + "\t\t" + acc.balance);
+        }
     }
 }
