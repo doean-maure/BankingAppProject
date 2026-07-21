a73553022c577b75e46ea747e6640a238413ede2
Doean Francimae Gilyn Maure <gilynmaure@gmail.com>
2026-07-22 01:02:19 +0800

    create balance and withdraw method in checking account

--- src/com/bank/models/CheckingAccount.java
+++ src/com/bank/models/CheckingAccount.java
@@ -1,5 +1,7 @@
 package com.bank.models;
 
+import java.util.InputMismatchException;
+
 public class CheckingAccount extends BankAccount {
     private static final double MIN_BALANCE = 100.0;
 
@@ -7,5 +9,23 @@ public class CheckingAccount extends BankAccount {
         super(accountNumner, initialBalance);
 
         this.balance = MIN_BALANCE;
+        
+    }
+
+     public void balance() {
+        System.out.println("\nCHECKING ACCOUNT BALANCE: P" + balance);
+    }
+
+    @Override
+    public boolean withdraw(double amount) {
+        try {
+             if (amount > 0 && amount <= balance) {
+                return true;
+            }
+        } catch (InputMismatchException e) {
+            // TODO: handle exception
+            System.out.println("\n***INSUFFICIENT FUNDS.***\n");
+        }
+        return false;
     }
 }
