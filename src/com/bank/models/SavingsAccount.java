7bd3b511f2e056682ad95c93150a60d48781e7e2
Doean Francimae Gilyn Maure <gilynmaure@gmail.com>
2026-07-22 01:01:26 +0800

    create withdraw and balance method in savings account

--- src/com/bank/models/SavingsAccount.java
+++ src/com/bank/models/SavingsAccount.java
@@ -1,5 +1,7 @@
 package com.bank.models;
 
+import java.util.InputMismatchException;
+
 public class SavingsAccount extends BankAccount {
     private static final double MIN_BALANCE = 100.0;
 
@@ -9,5 +11,20 @@ public class SavingsAccount extends BankAccount {
         this.balance = MIN_BALANCE;
     }
 
-    
+    public void balance() {
+        System.out.println("\nSAVINGS ACCOUNT BALANCE: P" + balance);
+    }
+
+    @Override
+    public boolean withdraw(double amount) {
+        try {
+             if (amount > 0 && amount <= balance) {
+                return true;
+            }
+        } catch (InputMismatchException e) {
+            // TODO: handle exception
+            System.out.println("\n***INSUFFICIENT FUNDS.***\n");
+        }
+        return false;
+    }
 }
